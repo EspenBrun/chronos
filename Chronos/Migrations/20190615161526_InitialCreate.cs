0b0b7bcdf7cebc2663b5cbacf87eec894eda6832
Espen Kirkesæther Brun <ekb@patientsky.com>
2019-06-15 18:30:13 +0200

    CH-1 EF migrations InitialCreate

---
+++ Chronos/Migrations/20190615161526_InitialCreate.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chronos.Migrations
+{
+    public partial class InitialCreate : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoItems",
+                columns: table => new
+                {
+                    Id = table.Column<string>(nullable: false),
+                    Name = table.Column<string>(nullable: false),
+                    Notes = table.Column<string>(nullable: false),
+                    Done = table.Column<bool>(nullable: false),
+                    Difficulty = table.Column<int>(nullable: false),
+                    Category = table.Column<string>(nullable: true),
+                    Tag = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoItems", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoItems");
+        }
+    }
+}
