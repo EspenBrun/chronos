5f7327ddcd2b66474f67e8887c85b65f7f72b687
Espen Kirkesæther Brun <ekb@patientsky.com>
2019-06-19 23:38:23 +0200

    CH-4 Ef migration for timeblock

---
+++ Chronos/Migrations/20190619213709_TimeBlock.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Chronos.Migrations
+{
+    public partial class TimeBlock : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "TimeBlocks",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    In = table.Column<DateTime>(nullable: false),
+                    Out = table.Column<DateTime>(nullable: false),
+                    Worked = table.Column<TimeSpan>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_TimeBlocks", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "TimeBlocks");
+        }
+    }
+}
